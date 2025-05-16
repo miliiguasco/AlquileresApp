eca06910694bad83e96eedcf2fe66f749abb0782
wiehl-valentina <valenwiehl@gmail.com>
2025-05-16 16:30:11 -0300

    add enumerativos

---
+++ AlquileresApp.Core/Enumerativos/Servicios.cs
@@ -0,0 +1,13 @@
+namespace AlquileresApp.Core.Enumerativos;
+
+public enum Servicios
+{
+    Wifi,
+    AireAcondicionado,
+    Calefaccion,
+    Cocina,
+    Lavadora,
+    Estacionamiento,
+    Piscina,
+    Television
+} 
