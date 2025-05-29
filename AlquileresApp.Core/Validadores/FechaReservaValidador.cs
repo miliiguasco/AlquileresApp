924ad81d934c39f47452d52e246010c527d59154
Manu <manupedrob@gmail.com>
2025-05-28 21:31:47 -0300

    tarjeta funciona

--- AlquileresApp.Core/Validadores/FechaReservaValidador.cs
+++ AlquileresApp.Core/Validadores/FechaReservaValidador.cs
@@ -5,26 +5,20 @@ namespace AlquileresApp.Core;
 public class FechaReservaValidador : IFechaReservaValidador
 {
     public void FechaValidador(DateTime fechaInicio, DateTime fechaFin) 
-        {
-            ValidarFechaReserva(fechaInicio, fechaFin);
-        }
-/*
-    public void ValidarFechaReserva(DateTime fechaInicio, DateTime fechaFin){
-        if (fechaInicio < DateTime.Now)
+    {
+        ValidarFechaReserva(fechaInicio, fechaFin);
+    }
+
+    public void ValidarFechaReserva(DateTime fechaInicio, DateTime fechaFin)
+    {
+        if (fechaInicio < DateTime.Today)
             throw new Exception("La fecha de inicio de la reserva no puede ser en el pasado");
             
-        if (fechaFin < DateTime.Now) 
+        if (fechaFin < DateTime.Today) 
             throw new Exception("La fecha de fin de la reserva no puede ser en el pasado");
             
-        if (fechaInicio > fechaFin)
-            throw new Exception("La fecha de inicio de la reserva no puede ser mayor a la fecha de fin");
-    }*/
-         public void ValidarFechaReserva(DateTime fechaInicio, DateTime fechaFin)
-        {
-            if (fechaInicio == null || fechaFin == null)
-        {
-            throw new Exception("Debe seleccionar una fecha de inicio y fin.");
-        }
+        if (fechaInicio >= fechaFin)
+            throw new Exception("La fecha de inicio de la reserva debe ser anterior a la fecha de fin");
     }
 }
 
